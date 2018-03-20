cb30bf73d88e025a13b499276103320a455b3903
Jared <meadowsjared@gmail.com>
2018-03-19 18:10:20 -0700

    implemented ngFor and string ineterpolation

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./market.component.css']
 })
 export class MarketComponent implements OnInit {
+	collectables = [
+		{ description: 'A very rare copy of "jQuery for Dummies"', type: 'Book' },
+		{ description: 'The first Letter ever written', type: 'Piece of Paper' },
+		{ description: 'A photograph showing nothing', type: 'Photo' },
+		{ description: 'A box with all sold Zunes', type: 'Garbage' }
+	];
+
+	onAddToCollection() {
+		alert('Hello')
+	}
 
 	constructor() { }
 
